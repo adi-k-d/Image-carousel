16e7b95834450a7dee3616ca2f82701cd74680c2
AdityaD <adityakdas7@gmail.com>
2021-10-12 09:49:10 +0530

    Just carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
 
 function App() {
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='container'>
+      <Header/>
+      <ImageSlider slides={SliderData} />;
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        
+            
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+export default header
